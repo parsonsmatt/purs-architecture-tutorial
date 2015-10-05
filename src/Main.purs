9fac78937fd145b4529a7f8d154d4b9aa412cdfb
Matt Parsons <parsonsmatt@gmail.com>
2015-10-05 19:47:35 -0400

    restart with Halogen

--- src/Main.purs
+++ src/Main.purs
@@ -5,21 +5,7 @@ import Prelude
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
 
-import qualified DOMImport as DOM
-
-import Data.Maybe
-import Control.Monad.Eff
-import Data.Maybe.Unsafe
-import Data.Nullable (toMaybe)
 import qualified Example.One as Ex1
 import qualified Example.Two as Ex2
 
-main =
-    documentBody >>= Ex2.renderCounter
-
-documentBody :: forall eff. Eff (dom :: DOM.DOM | eff) DOM.Element
-documentBody = do
-    win <- DOM.window
-    doc <- DOM.document win
-    elm <- fromJust <$> toMaybe <$> DOM.body doc
-    return $ DOM.htmlElementToElement elm
+main = log "wat"
