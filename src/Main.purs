d1ac15b4e320e3441fa5f6d0b5a3c1bea7159a62
Matt Parsons <parsonsmatt@gmail.com>
2015-10-05 01:26:30 -0400

    Finish Example One

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,27 @@
 module Main where
 
-import Control.Monad.Eff.Console
+import Prelude
 
-main = do
-  log "Hello sailor!"
+import Control.Monad.Eff
+import Control.Monad.Eff.Console
+import qualified DOM as DOM
+import qualified DOM.HTML as DOM
+import qualified DOM.HTML.Document as DOM
+import qualified DOM.HTML.Types as DOM
+import qualified DOM.HTML.Window as DOM
+import qualified DOM.Node.Types as DOM
+import Data.Maybe
+import Control.Monad.Eff
+import Data.Maybe.Unsafe
+import Data.Nullable (toMaybe)
+import Example.One
+
+main =
+    documentBody >>= renderCounter
+
+documentBody :: forall eff. Eff (dom :: DOM.DOM | eff) DOM.Element
+documentBody = do
+    win <- DOM.window
+    doc <- DOM.document win
+    elm <- fromJust <$> toMaybe <$> DOM.body doc
+    return $ DOM.htmlElementToElement elm
---
+++ src/example/one.purs
@@ -0,0 +1,50 @@
+module Example.One where
+
+import Prelude
+
+import Control.Monad.Eff
+
+import qualified Thermite as T
+import qualified Thermite.Action as T
+
+import qualified React as R
+import qualified React.DOM as R
+import qualified React.DOM.Props as RP
+
+import qualified DOM as DOM
+import qualified DOM.HTML as DOM
+import qualified DOM.HTML.Document as DOM
+import qualified DOM.HTML.Types as DOM
+import qualified DOM.HTML.Window as DOM
+
+type Model = Int
+
+data Action
+    = Increment
+    | Decrement
+
+update :: Action -> Model -> Model
+update Increment model =
+    model + 1
+update Decrement model =
+    model - 1
+
+view :: T.Render _ Model _ Action
+view send state _ _ =
+    R.div'
+        [ R.button [ RP.onClick \_ -> send Decrement ] 
+                   [ R.text "-" ]
+        , R.div'   [ R.text (show state) ]
+        , R.button [ RP.onClick \_ -> send Increment ] 
+                   [ R.text "+" ]
+        ]
+
+performAction :: T.PerformAction _ Model _ Action
+performAction _ action =
+    T.modifyState (update action)
+
+spec :: T.Spec _ Model _ Action
+spec = T.simpleSpec 0 performAction view
+
+renderCounter :: forall eff. _ -> Eff (dom :: DOM.DOM | eff) R.ReactElement
+renderCounter = R.render (R.createFactory (T.createClass spec) 0)
