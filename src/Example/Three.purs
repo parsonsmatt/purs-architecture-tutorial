03a6452de701066ba3a36fdc01fd9eeaea6dfbfd
Matt Parsons <parsonsmatt@gmail.com>
2015-10-07 16:52:18 -0400

    Example Three

---
+++ src/Example/Three.purs
@@ -0,0 +1,64 @@
+module Example.Three where
+
+import Prelude
+import Data.Array
+import Control.Plus (Plus)
+
+import Halogen
+import qualified Halogen.HTML.Indexed as H
+import qualified Halogen.HTML.Events.Indexed as E
+
+import qualified Example.Counter as Counter
+import Example.Two (CounterSlot(..))
+
+type State =
+    { counterList :: Array Int
+    , nextID :: Int
+    }
+
+initialState :: State
+initialState =
+    { counterList: []
+    , nextID: 0
+    }
+
+data Input a
+    = AddCounter a
+    | RemoveCounter a
+
+listUI :: forall g p. (Functor g)
+       => ParentComponent State Counter.State Input Counter.Input g CounterSlot p
+listUI = component render eval
+    where
+        render state = 
+            H.div_ [ H.h1_ [ H.text "Counters" ]
+                   , H.ul_ $ map (H.slot <<< CounterSlot) state.counterList
+                   , H.button [ E.onClick $ E.input_ AddCounter ]
+                              [ H.text "Add Counter" ]
+                   , H.button [ E.onClick $ E.input_ RemoveCounter ]
+                              [ H.text "Remove Counter" ]
+                   ]
+        eval :: EvalP Input State Counter.State Input Counter.Input g CounterSlot p
+        eval (AddCounter next) = do
+            modify addCounter
+            pure next
+        eval (RemoveCounter next) = do
+            modify removeCounter
+            pure next
+
+addCounter :: State -> State
+addCounter s =
+    s { counterList = s.nextID `cons` s.counterList
+      , nextID = s.nextID + 1
+      }
+
+removeCounter :: State -> State
+removeCounter s =
+    s { counterList = drop 1 s.counterList
+      , nextID = s.nextID - 1
+      }
+
+ui :: forall g p. (Plus g) => InstalledComponent State Counter.State Input Counter.Input g CounterSlot p
+ui = install listUI mkCounter
+    where
+        mkCounter (CounterSlot _) = createChild Counter.ui (Counter.init 0)
--- src/Main.purs
+++ src/Main.purs
@@ -11,14 +11,17 @@ import Control.Monad.Eff.Exception (throwException)
 import qualified Example.Zero as Ex0
 import qualified Example.One as Ex1
 import qualified Example.Two as Ex2
+import qualified Example.Three as Ex3
 
 --
 main = runAff throwException (const (pure unit)) $ do
     -- app <- runEx0
     -- app <- runEx1
-    app <- runEx2
+    -- app <- runEx2
+    app <- runEx3
     appendToBody app.node
 
 runEx0 = runUI Ex0.ui unit
 runEx1 = runUI Ex1.ui (Ex1.init 0)
 runEx2 = runUI Ex2.ui (installedState (Ex2.init))
+runEx3 = runUI Ex3.ui (installedState (Ex3.initialState))
