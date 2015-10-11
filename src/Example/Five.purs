85b42cab0ad33a746b96ce8b04aa9b6b324e5e00
Matt Parsons <parsonsmatt@gmail.com>
2015-10-10 23:26:35 -0400

    Five

---
+++ src/Example/Five.purs
@@ -0,0 +1,73 @@
+module Example.Five where
+
+import Prelude
+
+import Data.Foldable (mconcat)
+import Data.Either (either)
+import Control.Monad.Aff (Aff(), runAff)
+import Control.Monad.Eff
+import Control.Monad.Free (liftFI)
+import Data.Foreign.Class (readProp)
+import Network.HTTP.RequestHeader
+import Network.HTTP.MimeType.Common (applicationJSON)
+
+import Halogen
+import qualified Halogen.HTML.Indexed as H
+import qualified Halogen.HTML.Properties.Indexed as P
+import qualified Halogen.HTML.Events.Indexed as E
+
+import Network.HTTP.Affjax (AJAX())
+import qualified Network.HTTP.Affjax as AJ
+
+type State =
+  { topic :: String
+  , gifUrl :: String
+  }
+
+initialState :: State
+initialState = { topic: "cats", gifUrl: "" }
+
+data Input a
+  = RequestMore a
+
+type GifEffects eff = HalogenEffects (ajax :: AJAX | eff)
+
+giphyKey :: String
+giphyKey = "dc6zaTOxFJmzC"
+
+giphyUrl :: String
+giphyUrl = "https://api.giphy.com/v1/gifs/random"
+
+giphyRequestUrl :: String -> String
+giphyRequestUrl topic = mconcat
+  [ giphyUrl
+  , "?tag=", topic
+  , "&api_key=", giphyKey
+  ]
+
+ui :: forall p. Component State Input (Aff (GifEffects ())) p
+ui = component render eval
+  where
+    render :: Render State Input p
+    render state =
+      H.div_
+        [ H.h2_ [ H.text state.topic ]
+        , H.button [ E.onClick $ E.input_ RequestMore ]
+                   [ H.text "Moar!!" ]
+        , H.img [ P.src state.gifUrl ]
+        ]
+
+    eval :: Eval Input State Input (Aff (GifEffects ()))
+    eval (RequestMore a) = do
+      state <- get
+      newGifUrl <- liftFI (fetchGif state.topic)
+      modify \s -> if newGifUrl /= "error" 
+                      then s { gifUrl = newGifUrl  }
+                      else s { gifUrl = s.gifUrl }
+      pure a
+
+fetchGif :: forall eff. String -> Aff (ajax :: AJAX | eff) String
+fetchGif topic = do
+    result <- AJ.get (giphyRequestUrl topic)
+    let url = readProp "data" result.response >>= readProp "image_url"
+    pure (either (const "error") id url)
--- src/Example/One.purs
+++ src/Example/One.purs
@@ -7,32 +7,33 @@ import qualified Halogen.HTML.Indexed as H
 import qualified Halogen.HTML.Events.Indexed as E
 
 type State =
-    { count :: Int
-    }
+  { count :: Int
+  }
 
 data Input a
-    = Increment a
-    | Decrement a
+  = Increment a
+  | Decrement a
 
 init :: Int -> State
 init i = { count: i }
 
 ui :: forall g p. (Functor g) => Component State Input g p
 ui = component render eval
-    where
-        render :: Render State Input p
-        render state =
-            H.div_ [ H.button [ E.onClick $ E.input_ Decrement ] 
-                              [ H.text "-" ]
-                   , H.p_ [ H.text (show state.count)]
-                   , H.button [ E.onClick $ E.input_ Increment ] 
-                              [ H.text "+" ]
-                   ]
+  where
+    render :: Render State Input p
+    render state =
+      H.div_ 
+        [ H.button [ E.onClick $ E.input_ Decrement ] 
+                   [ H.text "-" ]
+        , H.p_ [ H.text (show state.count)]
+        , H.button [ E.onClick $ E.input_ Increment ] 
+                   [ H.text "+" ]
+        ]
 
-        eval :: Eval Input State Input g
-        eval (Increment next) = do
-            modify (\state -> state { count = state.count + 1 }) 
-            pure next
-        eval (Decrement next) = do
-            modify (\state -> state { count = state.count - 1 })
-            pure next
+    eval :: Eval Input State Input g
+    eval (Increment next) = do
+      modify (\state -> state { count = state.count + 1 }) 
+      pure next
+    eval (Decrement next) = do
+      modify (\state -> state { count = state.count - 1 })
+      pure next
